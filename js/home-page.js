3da9c4cc0fbf6719aadbbebe262edad080b18f32
Adekoya tolulope <114665531+Tolulope-Adekoya@users.noreply.github.com>
2023-07-03 15:30:31 +0100

    first commit

---
+++ js/home-page.js
@@ -0,0 +1,25 @@
+// FOR THE NAVIGATION BAR
+const scrollThreshold = 50; 
+//this is to Change the value to the desired scroll threshold in viewport width, it does not have to be 50, this just works foe me
+window.addEventListener('scroll', function() {
+  var object = document.getElementById('navColor');
+  var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+  var viewportWidth = window.innerwidth || document.documentElement.clientWidth;
+
+  if (scrollPosition > viewportWidth * (scrollThreshold / 100)) {
+    //if you just put the scrollposition it will automatically be taken a pixels 
+    //we tweaked the scroll threshhold to be at a preferable point
+    //using viewport heigt is confusing so we used viewport width
+    // doing the math ie is scrollposition greater than (vieportwitdh X 0.5%) cuz we used 50 for scroll threshold
+    object.style.backgroundColor = 'rgba(37, 95, 150, 0.9)';
+    object.style.transition = 'all 0.4s ease';
+  } else {
+    object.style.backgroundColor = 'rgba(3, 7, 10, 0.1)';
+  }
+});
+//FOR THE PRICE SLIDER
+// JavaScript function to update the displayed value
+function updateValue(value) {
+  document.getElementById("rangeValue").
+  textContent = value;  
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# TRAVELGEM ✈️
+
+This project was completed using <b>html, java script, bootstrap and css</b>
+
+This website serves as a digital representation of a fictional reputable travel agency, offering users the opportunity to book a wide array of destinations tailored to their budgetary preferences. While currently static in nature, the website holds the potential for future expansion into a dynamic platform.
+
+Functioning as a comprehensive template, the website effectively showcases the agency's core offerings, providing customers with a seamless platform to access reviews and sample offerings from numerous resorts available for rental through the agency.
+
+The link to the ui design is [ here](https://www.behance.net/fagbohutemilol)
+
+### The team 🧑‍🤝‍🧑:
+
+>* [Fagbohun Temilolouwa](https://www.behance.net/gallery/169519183/Travelgem) (uiux designer)
+
+>* [Adekoya Tolulope](https://github.com/Tolulope-Adekoya) (lead developer)
+
+>* [Oluwaseun ogundeko](https://github.com/iamunik)
+
